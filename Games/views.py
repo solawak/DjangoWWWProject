bbb5552733ab6e2ff5fe36b7dc5c7e53dbf5b3a4
MattS0000 <mati201420@gmail.com>
2021-11-15 13:36:38 +0100

    Scoreboard added

--- Games/views.py
+++ Games/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Scores
 from django.http import HttpResponse
 
 
@@ -10,19 +11,30 @@ def index(request):
     return render(request, 'Games/index.html')
 
 
-def game1(request):
-    context = {'game_name': 'Game1'}
+def game_render(request, game_name):
+    scoreboard_sorted = Scores.objects.filter(game=game_name).order_by('-score')
+    scoreboard_top10 = scoreboard_sorted[:10]
+    if request.user.is_authenticated:
+        user_scores = scoreboard_sorted.filter(user=request.user)[:10]
+    else:
+        user_scores = []
+    context = {'game_name': game_name, 'scoreboard': scoreboard_top10, 'user_scores': user_scores}
     return render(request, 'Games/game_template.html', context)
+
+
+def game1(request):
+    game_name = 'Snake'
+    return game_render(request, game_name)
 
 
 def game2(request):
-    context = {'game_name': 'Game2'}
-    return render(request, 'Games/game_template.html', context)
+    game_name = 'Flappy_Bird'
+    return game_render(request, game_name)
 
 
 def game3(request):
-    context = {'game_name': 'Game3'}
-    return render(request, 'Games/game_template.html', context)
+    game_name = 'Blank'
+    return game_render(request, game_name)
 
 
 def register_request(request):
