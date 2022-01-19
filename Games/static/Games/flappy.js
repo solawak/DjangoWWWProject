d03be7c5628480fdc4872e2f8d47a9785f64d792
MattS0000 <mati201420@gmail.com>
2022-01-19 15:25:07 +0100

    html5 corrections

--- Games/static/Games/flappy.js
+++ Games/static/Games/flappy.js
@@ -7,6 +7,7 @@ var myScore;
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
+        this.canvas.setAttribute("id", "flappycanva");
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
